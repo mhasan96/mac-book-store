7408b38904c15e10711ce5641d68c2fa78d9c789
Munnass95 <mehedihasan162120@gmail.com>
2021-08-21 23:10:38 +0600

    full project Done

--- js/app.js
+++ js/app.js
@@ -1,75 +1,71 @@
 // Memory Calculations
-
-function addMemory(isTrue) {
-  const memoryInput = document.getElementById("extra-memory");
-  let extraMemoryCost = parseInt(memoryInput.innerText);
-
-  if (isTrue == false) {
-    extraMemoryCost = 0;
-    memoryInput.innerText = extraMemoryCost;
-  } else if (isTrue == true) {
-    extraMemoryCost = 180;
-    memoryInput.innerText = extraMemoryCost;
-  }
+function modifyConfig(product, price) {
+  const memoryInput = document.getElementById(product);
+  let extraMemoryCost = memoryInput.innerText;
+  extraMemoryCost = price;
+  memoryInput.innerText = extraMemoryCost;
+  calculateTotal();
 }
 
+// Total Price Calculation
+function calculateTotal() {
+  const bestPrice = 1299;
+  const memory = parseInt(document.getElementById("extra-memory").innerText);
+  const storage = parseInt(document.getElementById("extra-storage").innerText);
+  const delivery = parseInt(
+    document.getElementById("delivery-charges").innerText
+  );
+  const calculateTotalPrice = bestPrice + memory + storage + delivery;
+  document.getElementById("total-price").innerText = calculateTotalPrice;
+  calculation();
+}
+
+// Promo Code Calculation
+document.getElementById("promo-button").addEventListener("click", function () {
+  calculation("promo-button");
+});
+function getInputValue() {
+  const totalPriceInput = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceInput.innerText);
+  return totalPrice;
+}
+function calculation() {
+  const inputText = document.getElementById("promo-input");
+  const input = inputText.value;
+  let totalPriceText = document.getElementById("total");
+  let totalPrice = totalPriceText.innerText;
+  totalPrice = getInputValue();
+  totalPriceText.innerText = totalPrice;
+  const discount = totalPrice / 5;
+  const finalDiscount = totalPrice - discount;
+  if (input == "stevekaku") {
+    document.getElementById("promo-button").disabled = true;
+    totalPriceText.innerText = finalDiscount;
+  }
+  inputText.value = "";
+}
+// call the function
 document.getElementById("memory-8").addEventListener("click", function () {
-  addMemory(false);
+  modifyConfig("extra-memory", 0);
 });
 document.getElementById("memory-16").addEventListener("click", function () {
-  addMemory(true);
+  modifyConfig("extra-memory", 180);
 });
-
-// Storage Calculations
-
-function addStorage(storageSize) {
-  const storageInput = document.getElementById("extra-storage");
-  let exrtaStorageCost = parseInt(storageInput.innerText);
-
-  if (storageSize == "storage-256") {
-    exrtaStorageCost = 0;
-    storageInput.innerText = exrtaStorageCost;
-  } else if (storageSize == "storage-512") {
-    exrtaStorageCost = 100;
-    storageInput.innerText = exrtaStorageCost;
-  } else if (storageSize == "storage-1TB") {
-    exrtaStorageCost = 180;
-    storageInput.innerText = exrtaStorageCost;
-  }
-}
 
 document.getElementById("storage-256").addEventListener("click", function () {
-  addStorage("storage-256");
+  modifyConfig("extra-storage", 0);
 });
 document.getElementById("storage-512").addEventListener("click", function () {
-  addStorage("storage-512");
+  modifyConfig("extra-storage", 100);
 });
 document.getElementById("storage-1TB").addEventListener("click", function () {
-  addStorage("storage-1TB");
+  modifyConfig("extra-storage", 180);
 });
-
-// Delivery Charges Calculaitons
-
-function addDelivery(isTrue) {
-  const deliveryInput = document.getElementById("delivery-charges");
-  let deliveryCost = parseInt(deliveryInput.innerText);
-  if (isTrue == true) {
-    deliveryCost = 0;
-    deliveryInput.innerText = deliveryCost;
-  } else if (isTrue == false) {
-    deliveryCost = 20;
-    deliveryInput.innerText = deliveryCost;
-  }
-}
-
 document.getElementById("delivery-free").addEventListener("click", function () {
-  addDelivery(true);
+  modifyConfig("delivery-charges", 0);
 });
-
 document
   .getElementById("delivery-not-free")
   .addEventListener("click", function () {
-    addDelivery(false);
+    modifyConfig("delivery-charges", 20);
   });
-
-// Total price Calculation
